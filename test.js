8e13a22fa9c155772b843f3cbaf12df574398099
mhkeller <code@mhkeller.com>
2016-07-25 18:19:32 -0400

    proper tests

--- test.js
+++ test.js
@@ -1,23 +1,24 @@
-var assert = require('chai').assert
+var chai = require('chai')
+
+var assert = chai.assert
+var expect = chai.expect
 
 var latestTag = require('./')
 
 describe('latestTag', function () {
   var stableTag = 'v1.0.0'
   it('should return the given tag: ' + stableTag, function () {
-    latestTag('mhkeller', 'stable-tag').then(function (tag) {
-      assert.equal(stableTag, tag)
-    })
-    .catch(function (err) {
-      console.error(err)
+    return latestTag('mhkeller', 'stable-tag').then(tag => {
+      expect(tag).to.equal(stableTag)
     })
   })
   it('should return the proper error for no tags', function () {
-    latestTag('mhkeller', 'stable-tag').then(function (tag) {
-      console.log(tag)
-    })
-    .catch(function (err) {
-      assert.equal(err, new Error('No tags found for ' + user + '/' + repo))
+    var username = 'mhkeller'
+    var repo = 'no-tags'
+    var error = new Error('No tags found for ' + username + '/' + repo)
+    return latestTag(username, repo).catch(err => {
+      expect(err.name).to.equal(error.name)
+      expect(err.message).to.equal(error.message)
     })
   })
 })
